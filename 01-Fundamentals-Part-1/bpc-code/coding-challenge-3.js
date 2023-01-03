78f2663770e0594b9a65a81d683830cfcc203f41
buentellopc <buentellopc@gmail.com>
2023-01-03 14:34:41 -0600

    coding challenge 3

---
+++ 01-Fundamentals-Part-1/bpc-code/coding-challenge-3.js
@@ -0,0 +1,16 @@
+dolphinsAvg = (107 + 112 + 81) / 3;
+console.log(dolphinsAvg);
+
+koalasAvg = (109 + 95 + 96) / 3;
+console.log(koalasAvg);
+
+// compare to see the winner
+if ((dolphinsAvg == koalasAvg) & (dolphinsAvg >= 100) & (koalasAvg >= 100)) {
+  console.log("draw");
+} else if ((dolphinsAvg > koalasAvg) & (dolphinsAvg > 100)) {
+  console.log("Dolphins win");
+} else if ((koalasAvg > dolphinsAvg) & (koalasAvg > 100)) {
+  console.log("Koalas win");
+} else {
+  console.log("No one wins the thropy");
+}
