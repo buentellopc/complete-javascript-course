130bc40a5ad9fdb65d7ae4b3a6840bcd99122f5b
buentellopc <buentellopc@gmail.com>
2023-01-03 14:58:24 -0600

    coding challenge 4 done take 2

--- 01-Fundamentals-Part-1/bpc-code/coding-challenge-4.js
+++ 01-Fundamentals-Part-1/bpc-code/coding-challenge-4.js
@@ -1,16 +1,9 @@
 // Declare variables
 const bill = 275;
-let tip = 0;
-let totalValue = 0;
-// Calculate the total value
-// const totalValue = bill + bill * tip;
-
-bill >= 50 && bill <= 300 ? (tip = 0.15) : (tip = 0.2);
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
 totalValue = bill + bill * tip;
 
 console.log(
-  `The bill was ${bill}, the tip was ${
-    bill * tip
-  }, and the total value ${totalValue}`
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
 );
