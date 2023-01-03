ef04bcc3a3fd86be48e6261c6c45307214fdf184
buentellopc <buentellopc@gmail.com>
2023-01-03 15:36:11 -0600

    use logical rather than bitwise operators

--- 01-Fundamentals-Part-1/bpc-code/coding-challenge-3.js
+++ 01-Fundamentals-Part-1/bpc-code/coding-challenge-3.js
@@ -5,11 +5,11 @@ koalasAvg = (109 + 95 + 96) / 3;
 console.log(koalasAvg);
 
 // compare to see the winner
-if ((dolphinsAvg == koalasAvg) & (dolphinsAvg >= 100) & (koalasAvg >= 100)) {
+if (dolphinsAvg == koalasAvg && (dolphinsAvg >= 100) & (koalasAvg >= 100)) {
   console.log("draw");
-} else if ((dolphinsAvg > koalasAvg) & (dolphinsAvg > 100)) {
+} else if (dolphinsAvg > koalasAvg && dolphinsAvg > 100) {
   console.log("Dolphins win");
-} else if ((koalasAvg > dolphinsAvg) & (koalasAvg > 100)) {
+} else if (koalasAvg > dolphinsAvg && koalasAvg > 100) {
   console.log("Koalas win");
 } else {
   console.log("No one wins the thropy");
