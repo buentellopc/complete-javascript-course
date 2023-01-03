fc32cf679767257845b29a5d0f5fa9114da3af85
buentellopc <buentellopc@gmail.com>
2023-01-02 22:16:03 -0600

    first commit in inspiron

--- 01-Fundamentals-Part-1/starter/script.js
+++
@@ -1,7 +0,0 @@
-// This is the first comment, the first commit and the start of the journey!
-// This is the first comment, the first commit and the start of the journey!
-// This is the first comment, the first commit and the start of the journey!
-// This is the first comment, the first commit and the start of the journey!
-// This is the first comment, the first commit and the start of the journey!
-// This is the first comment, the first commit and the start of the journey!
-// This is the first comment, the first commit and the start of the journey!
